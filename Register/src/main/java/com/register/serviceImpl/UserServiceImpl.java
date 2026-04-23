7c68d3fb56f41454b9329dc2e96a38fe1bc691d2
unknown <sumitswami5051@gmail.com>
2026-04-23 10:22:59 +0530

    demo 1

--- Register/src/main/java/com/register/serviceImpl/UserServiceImpl.java
+++ Register/src/main/java/com/register/serviceImpl/UserServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import java.util.Optional;
+
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -19,5 +22,40 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(user);
     }
 
+
+    @Override
+    public List<User> getAllUsers() {
+
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @Override
+    public User updateUser(Long userId, User user) {
+        Optional<User> oUser= userRepository.findById(userId);
+        if(oUser.isPresent()){
+            User user2=oUser.get();
+            user2.setDob(user.getDob());
+            user2.setEmail(user.getEmail());
+            user2.setGender(user.getGender());
+            user2.setPassword(user.getPassword());
+            user2.setUpdatedate(user.getUpdatedate());
+            user2.setPhoneNo(user.getPhoneNo());
+            user2.setCreatedDate(user.getCreatedDate());
+
+           return userRepository.save(user2);
+        }
+        return null;
+    }
+
+    @Override
+    public String deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+        return  userId+" is deleted successfully";
+    }
 
 }
