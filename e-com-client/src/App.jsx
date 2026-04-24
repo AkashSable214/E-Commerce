1a3cf6f5a51f15d41fafb068b69eb74623e99ea4
itmarko <bhagwan13397@gmail.com>
2026-04-24 10:40:32 +0530

    update readme file

--- e-com-client/src/App.jsx
+++ e-com-client/src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "@/pages/Register";
-
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import ErrorPage from "@/pages/ErrorPage";
 const App = () => {
   return (
     <>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-      {/* Catch-all route for 404 */}
-      <Route path="*" element={<ErrorPage />} />
+          {/* Catch-all route for 404 */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </>
   );
 };
