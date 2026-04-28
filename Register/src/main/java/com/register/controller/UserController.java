944e9b93311d8d301f3bcd8a59bb8a200b714c79
AkashSable214 <akashsable576@gmail.com>
2026-04-28 17:31:26 +0530

    updated code

--- Register/src/main/java/com/register/controller/UserController.java
+++ Register/src/main/java/com/register/controller/UserController.java
@@ -1,79 +1,70 @@
 package com.register.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.register.model.User;
 import com.register.model.UserInvoice;
 import com.register.service.UserInvoiceService;
 import com.register.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 @CrossOrigin("http://localhost:5173/")
 @RestController
-@RequestMapping(value="/userApi/v1")
+@RequestMapping(value = "/userApi/v1")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private UserInvoiceService userInvoiceService;
+	@Autowired
+	private UserInvoiceService userInvoiceService;
 
-    @PostMapping("/saveUser")
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        User savedUser = userService.saveUser(user);
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-    }
+	@PostMapping("/saveUser")
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		User savedUser = userService.saveUser(user);
+		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+	}
 
+	@GetMapping("/getUser/{userId}")
+	public ResponseEntity<User> getUser(@PathVariable long userId) {
+		User user = userService.getUserById(userId);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
 
-    @GetMapping("/getUser/{userId}")
-    public ResponseEntity<User> getUser(@PathVariable long userId) {
-        User user = userService.getUserById(userId);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
+	@GetMapping("/getAllUsers")
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> users = userService.getAllUsers();
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
 
+	@PutMapping("/updateUser/{userId}")
+	public ResponseEntity<User> updateUser(@PathVariable long userId, @RequestBody User user) {
 
-    @GetMapping("/getAllUsers")
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
+		User updatedUser = userService.updateUser(userId, user);
+		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+	}
 
+	@DeleteMapping("/deleteUser/{userId}")
+	public ResponseEntity<String> deleteUser(@PathVariable long userId) {
+		userService.deleteUserById(userId);
+		return new ResponseEntity<>("User deleted successfully", HttpStatus.NOT_FOUND);
+	}
 
-    @PutMapping("/updateUser/{userId}")
-    public ResponseEntity<User> updateUser(
-            @PathVariable long userId,
-            @RequestBody User user) {
-
-        User updatedUser = userService.updateUser(userId, user);
-        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/deleteUser/{userId}")
-    public ResponseEntity<String> deleteUser(@PathVariable long userId) {
-        userService.deleteUserById(userId);
-        return new ResponseEntity<>("User deleted successfully", HttpStatus.NOT_FOUND);
-    }
-
-<<<<<<< HEAD
-//    @PostMapping("/users/invoice")
-//    public ResponseEntity<String> saveInvoiceReference(
-//            @RequestParam Long invoiceId,
-//            @RequestParam Long userId) {
-//
-//        return ResponseEntity.ok("Saved");
-//    }
-
-    @PostMapping("/invoice/save")
-    public void saveInvoiceReference(@RequestBody UserInvoice userInvoice){
-        userInvoiceService.saveInvoice(userInvoice);
-    }
-=======
-
->>>>>>> 0a3d04e91875f7466c6f7d9b427cb1302c2264e6
+	@PostMapping("/invoice/save")
+	public void saveInvoiceReference(@RequestBody UserInvoice userInvoice) {
+		userInvoiceService.saveInvoice(userInvoice);
+	}
 
 }
